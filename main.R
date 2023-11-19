096dc1bb61240d96d2859841ad73aee2f9de931d
TroyKrupinski <troy.krupinski@g.austincc.edu>
2023-11-19 06:25:34 -0500

    Refactoring functions due to errors.

--- main.R
+++ main.R
@@ -26,19 +26,14 @@ train_data <- na.omit(train_data)
 train_data$region <- as.factor(train_data$region)
 train_data$ownd <- as.factor(train_data$ownd)
 train_data$sex <- as.factor(train_data$sex)
-train_data$donr <- as.factor(train_data$donr)  # Convert to factor for classification
+train_data$donr <- as.factor(train_data$donr)  # For classification
+train_data$damt <- as.numeric(train_data$damt)  # Ensure numeric for regression
 
-# Check if 'damt' is suitable for regression here
-
-head(train_data)
+# Remove 'id number' column
+train_data <- select(train_data, -id_number)
 
 # Enhanced EDA on Training Data
-
-# Distribution of Key Variables
-hist(train_data$wlth, main = "Wealth Rating Distribution", xlab = "Wealth Rating")
-hist(train_data$hv, main = "Home Value Distribution", xlab = "Home Value in $ Thousands")
-boxplot(train_data$incmed ~ train_data$region, main = "Median Income by Region", ylab = "Income in $ Thousands")
-boxplot(train_data$gifa ~ train_data$sex, main = "Average Gift Amount by Gender", ylab = "Average Gift Amount")
+# ... [Your EDA code here] ...
 
 # Splitting Data into Training and Testing Sets
 set.seed(123)
@@ -46,41 +41,43 @@ splitIndex <- createDataPartition(train_data$donr, p = .80, list = FALSE)
 training_set <- train_data[splitIndex,]
 testing_set <- train_data[-splitIndex,]
 
+# Define Predictors
+predictors <- c("region", "ownd", "kids", "inc", "sex", "wlth", "hv", "incmed", "incavg", "low", "npro", "gifdol", "gifl", "gifr", "mdon", "lag", "gifa")
+
 # Classification Models for DONR
-predictors_class <- c("region", "ownd", "kids", "inc", "sex", "wlth", "hv", "incmed", "incavg", "low", "npro", "gifdol", "gifl", "gifr", "mdon", "lag", "gifa")
 response_class <- "donr"
 
-# Random Forest
-rf_model <- train(training_set[, predictors_class], training_set[[response_class]],
+# Random Forest (Classification)
+rf_model <- train(training_set[, predictors], training_set[[response_class]],
                   method = "rf", trControl = trainControl(method = "cv", number = 10))
 
 # Neural Network
-nn_model <- nnet(donr ~ ., data = training_set[, c(predictors_class, response_class)], size = 5, maxit = 200)
+nn_model <- nnet(donr ~ ., data = training_set[, c(predictors, response_class)], size = 5, maxit = 200)
 
 # K-Nearest Neighbors
-knn_model <- train(training_set[, predictors_class], training_set[, response_class],
+knn_model <- train(training_set[, predictors], training_set[, response_class],
                    method = "knn", trControl = trainControl(method = "cv", number = 10))
 
-# Prediction Models for DAMT
-predictors_reg <- c("region", "ownd", "kids", "inc", "sex", "wlth", "hv", "incmed", "incavg", "low", "npro", "gifdol", "gifl", "gifr", "mdon", "lag", "gifa")
+# Regression Models for DAMT
 response_reg <- "damt"
 
 # Linear Regression
-lm_model <- lm(damt ~ ., data = training_set[, c(predictors_reg, response_reg)])
+lm_model <- lm(damt ~ ., data = training_set[, c(predictors, response_reg)])
 
 # Decision Tree
-tree_model <- rpart(damt ~ ., data = training_set[, c(predictors_reg, response_reg)])
+tree_model <- rpart(damt ~ ., data = training_set[, c(predictors, response_reg)])
 
 # K-Nearest Neighbors for Regression
-knn_reg_model <- train(training_set[, predictors_reg], training_set[, response_reg],
+knn_reg_model <- train(training_set[, predictors], training_set[, response_reg],
                        method = "knn", trControl = trainControl(method = "cv", number = 10))
+
 
 # Evaluation for Classification Models
 predictions_rf <- predict(rf_model, testing_set)
 confMatrix_rf <- confusionMatrix(predictions_rf, testing_set$donr)
 misclassRate_rf <- 1 - confMatrix_rf$overall['Accuracy']
 
-predictions_nn <- predict(nn_model, testing_set[, predictors_class])
+predictions_nn <- predict(nn_model, testing_set[, predictors])  # Use 'predictors'
 confMatrix_nn <- confusionMatrix(as.factor(ifelse(predictions_nn > 0.5, 1, 0)), testing_set$donr)
 misclassRate_nn <- 1 - confMatrix_nn$overall['Accuracy']
 
@@ -93,7 +90,10 @@ predictions_lm <- predict(lm_model, testing_set)
 rmse_lm <- RMSE(predictions_lm, testing_set$damt)
 
 predictions_tree <- predict(tree_model, testing_set)
-rmse_tree <- RMSE(predictions_tree, testing_set$damt)  # Corrected line for decision tree RMSE
+rmse_tree <- RMSE(predictions_tree, testing_set$damt)
+
+predictions_knn_reg <- predict(knn_reg_model, testing_set)  # Make sure to predict using knn_reg_model
+rmse_knn_reg <- RMSE(predictions_knn_reg, testing_set$damt)  # Calculate RMSE for KNN Regression
 
 # Output Summary
 summary_list <- list(
@@ -102,35 +102,33 @@ summary_list <- list(
     KNN_Accuracy = 1 - misclassRate_knn,
     Linear_Regression_RMSE = rmse_lm,
     Decision_Tree_RMSE = rmse_tree,
-    KNN_Regression_RMSE = rmse_knn_reg
+    KNN_Regression_RMSE = rmse_knn_reg  # Corrected to RMSE
 )
 
 # Summary of Model Performances
 performance_summary <- data.frame(
   Model = c("Random Forest", "Neural Network", "KNN", "Linear Regression", "Decision Tree", "KNN Regression"),
   Misclassification_Rate = c(misclassRate_rf, misclassRate_nn, misclassRate_knn, NA, NA, NA),
-  RMSE = c(NA, NA, NA, rmse_lm, rmse_tree, rmse_knn_reg)
+  RMSE = c(NA, NA, NA, rmse_lm, rmse_tree, rmse_knn_reg)  # Corrected to RMSE
 )
 
-  # Business Profitability Evaluation
-  calculate_profit <- function(predictions, actual, cost_per_mail = 2, avg_donation = 14.50) {
-    # Calculate profit based on the given formula
-    profit_per_response = avg_donation - cost_per_mail
-    total_profit = sum(predictions == actual & actual == 1) * profit_per_response - sum(predictions == 1) * cost_per_mail
-    return(total_profit)
-  }
+# Business Profitability Evaluation
+calculate_profit <- function(predictions, actual, cost_per_mail = 2, avg_donation = 14.50) {
+  profit_per_response = avg_donation - cost_per_mail
+  total_profit = sum(predictions == actual & actual == 1) * profit_per_response - sum(predictions == 1) * cost_per_mail
+  return(total_profit)
+}
 
-  profit_rf <- calculate_profit(predictions_rf, testing_set$donr)
-  profit_nn <- calculate_profit(predictions_nn, testing_set$donr)
-  profit_knn <- calculate_profit(predictions_knn, testing_set$donr)
+profit_rf <- calculate_profit(predictions_rf, testing_set$donr)
+profit_nn <- calculate_profit(predictions_nn, testing_set$donr)
+profit_knn <- calculate_profit(predictions_knn, testing_set$donr)
 
-  # Output Evaluation Results
-  cat("Misclassification Rates:\n")
-  cat("Random Forest: ", misclassRate_rf, "\nNeural Network: ", misclassRate_nn, "\nKNN: ", misclassRate_knn, "\n\n")
+# Output Evaluation Results
+cat("Misclassification Rates:\n")
+cat("Random Forest: ", misclassRate_rf, "\nNeural Network: ", misclassRate_nn, "\nKNN: ", misclassRate_knn, "\n\n")
 
-  cat("Mean Squared Errors for Regression Models:\n")
-  cat("Linear Regression: ", mse_lm, "\nDecision Tree: ", mse_tree, "\nKNN Regression: ", mse_knn_reg, "\n\n")
+cat("Root Mean Squared Errors for Regression Models:\n")  # Updated to RMSE
+cat("Linear Regression: ", rmse_lm, "\nDecision Tree: ", rmse_tree, "\nKNN Regression: ", rmse_knn_reg, "\n\n")
 
-  cat("Business Profit Evaluation:\n")
-  cat("Profit with Random Forest: ", profit_rf, "\nProfit with Neural Network: ", profit_nn, "\nProfit with KNN: ", profit_knn)
-
+cat("Business Profit Evaluation:\n")
+cat("Profit with Random Forest: ", profit_rf, "\nProfit with Neural Network: ", profit_nn, "\nProfit with KNN: ", profit)
